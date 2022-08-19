779d9c0d0e8e6a8565e5a2c2618dc3fcb2540d10
Joaquín Carreño Rodríguez <joaquin2010233@gmail.com>
2022-08-19 18:31:32 -0500

    Se esta instalando las bases para empezar a construir el proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ToastrModule } from 'ngx-toastr';
+import { CVComponent } from './Components/cv/cv.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', 
+    redirectTo: 'CV',
+    pathMatch: 'full'
+  },
+  {
+    path: 'CV',
+    component: CVComponent
+  },
+  {
+    path: '***',
+    redirectTo: 'CV',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    ToastrModule.forRoot(), // ToastrModule added
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CVComponent } from './Components/cv/cv.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CVComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ToastrModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
