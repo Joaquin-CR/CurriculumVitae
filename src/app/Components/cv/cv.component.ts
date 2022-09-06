2677590517b21453296eec8c039292336eb4e8dd
Joaquín Carreño Rodríguez <joaquin2010233@gmail.com>
2022-09-05 21:08:39 -0500

    Se agregaron los iconos a utilizar para contactar al usuario
    Falta de checar el número telefónico

--- src/app/Components/cv/cv.component.ts
+++ src/app/Components/cv/cv.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { faGithub, faWhatsapp, faTelegram, faLinkedin, faGoogle, faWhatsappSquare} from '@fortawesome/free-brands-svg-icons'
+//import { fa } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-cv',
@@ -7,6 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CVComponent implements OnInit 
 {
+  faGithub = faGithub;
+  faWhatsapp = faWhatsapp;
+  faTelegram = faTelegram;
+  faLinkedin = faLinkedin;
+  faGoogle = faGoogle;
+  faWhatsappSquare = faWhatsappSquare;
 
   constructor() 
   { 
