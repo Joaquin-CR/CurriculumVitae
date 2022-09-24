41e475bb5e433423cb99a073513986b82c70d55f
Joaquín Carreño Rodríguez <joaquin2010233@gmail.com>
2022-09-24 18:47:39 -0500

    Se acomodó de mejor manera la parte superior del cv

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ToastrModule } from 'ngx-toastr';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { CVComponent } from './Components/cv/cv.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ExperienciaLaboralComponent } from './Components/experiencia-laboral/experiencia-laboral.component';
 import { HistorialAcademicoComponent } from './Components/historial-academico/historial-academico.component';
 import { HobbiesComponent } from './Components/hobbies/hobbies.component';
